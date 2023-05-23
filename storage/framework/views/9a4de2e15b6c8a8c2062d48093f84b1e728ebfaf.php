893a9b2dbae7568211f373b51a54cf828ee0bb00
blackg820 <blackg820@gmail.com>
2023-05-23 13:59:45 +0300

    d

--- storage/framework/views/9a4de2e15b6c8a8c2062d48093f84b1e728ebfaf.php
+++
@@ -1,3 +0,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-</svg><?php /**PATH /var/www/cars/storage/framework/views/bbf8517f99aecf95effc19b0872b6572150e8111.blade.php ENDPATH**/ ?>
